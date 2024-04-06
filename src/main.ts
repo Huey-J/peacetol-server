5ec78a62a6e3059041232be78ef23a262084cf53
Minji Park <gbsmj0312@gmail.com>
2024-04-06 11:42:01 +0900

    Init

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = 8000;
+
+  // OpenAPI(Swagger) 설정
+  const config = new DocumentBuilder()
+    .setTitle('Peacetol')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port);
+  console.log(`listening on port ${port}`);
+}
+bootstrap();
