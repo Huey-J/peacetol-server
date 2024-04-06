0e3d89dfc5d213b2da0a3ba164b0d663dd74608e
Huey-J <wkdgus7113@gmail.com>
2024-04-07 01:43:56 +0900

    feat: adventure 다음 단계 생성 API 추가

--- src/adventure/adventure.service.ts
+++ src/adventure/adventure.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateAdventureDto } from './dto/create.adventure.request';
 import { AdventureRepository } from './adventure.repository';
 import { UserRepository } from '../user/user.repository';
-import { PrismaClient } from '@prisma/client';
+import { Adventure, AnswerType, Mission, PrismaClient } from '@prisma/client';
 import { AdventureResponseDto, MissionDto } from './dto/adventure.response';
 import { AddNextStepForAdventureDto } from './dto/add.next.step.for.adventure.request';
 import { MissionRepository } from './mission.repository';
@@ -45,6 +45,7 @@ export class AdventureService {
 
     const createdAdventure = await this.adventureRepository.createAdventure(difficulty, userId);
 
+    // 1번 미션의 isTransportation 따라 2번 3번 미션 달라져야 함
     for (let step = 1; step < 4; step++) {
       const templates = await this.prisma.missionTemplate.findMany({
         where: { step },
@@ -71,27 +72,63 @@ export class AdventureService {
   async addNextStep(adventureId: string, addNextStepForAdventureDto: AddNextStepForAdventureDto): Promise<AdventureCreationResponseDto> {
     const {userUuid, answerType} = addNextStepForAdventureDto;
 
-    const adventure = this.adventureRepository.getById(parseInt(adventureId));
-    const userId = await this.userRepository.findUser(userUuid);
+    const adventure: Adventure = await this.adventureRepository.getById(parseInt(adventureId));
+    const userId: number = await this.userRepository.findUser(userUuid);
 
-    // todo await 빼기...
-    if ((await adventure).userId !== userId) {
-      // todo exception
+    if (adventure.userId !== userId) {
+      // todo exception...
       throw new Error('Not your adventure.');
     }
 
-    // bus 일 경우
+    // const firstMission = await this.missionRepository.findByAdventureIdAndStep(parseInt(adventureId), 1);
 
-    // 아닐 경우
+    const templateFiveList = await this.prisma.missionTemplate.findMany({
+      where: { step: 5, answerType: answerType },
+    });
+    const selectedFiveTemplate = templateFiveList[Math.floor(Math.random() * templateFiveList.length)];
+    const missionFive = await this.prisma.mission.create({
+      data: {
+        step: selectedFiveTemplate.step,
+        body: selectedFiveTemplate.body.replace('${number}', '' + (Math.floor(Math.random() * selectedFiveTemplate.endNumber) + 1)),
+        quote: selectedFiveTemplate.quote,
+        imagePath: selectedFiveTemplate.imagePath,
+        isTransportation: selectedFiveTemplate.isTransportation,
+        adventureId: adventure.id,
+      },
+    });
 
+    const templateSixList = await this.prisma.missionTemplate.findMany({
+      where: { step: 6 },
+    });
+    const selectedSixTemplate = templateSixList[Math.floor(Math.random() * templateSixList.length)];
+    const missionSix = await this.prisma.mission.create({
+      data: {
+        step: selectedSixTemplate.step,
+        body: selectedSixTemplate.body.replace('${number}', '' + (Math.floor(Math.random() * selectedSixTemplate.endNumber) + 1)),
+        quote: selectedSixTemplate.quote,
+        imagePath: selectedSixTemplate.imagePath,
+        isTransportation: selectedSixTemplate.isTransportation,
+        adventureId: adventure.id,
+      },
+    });
 
-    // type으로 필터링해서 missionTemplate 조회
+    const templateSevenList = await this.prisma.missionTemplate.findMany({
+      where: { step: 7 },
+    });
+    const selectedSevenTemplate = templateSevenList[Math.floor(Math.random() * templateSevenList.length)];
+    const missionSeven = await this.prisma.mission.create({
+      data: {
+        step: selectedSevenTemplate.step,
+        body: selectedSevenTemplate.body.replace('${number}', '' + (Math.floor(Math.random() * selectedSevenTemplate.endNumber) + 1)),
+        quote: selectedSevenTemplate.quote,
+        imagePath: selectedSevenTemplate.imagePath,
+        isTransportation: selectedSevenTemplate.isTransportation,
+        adventureId: adventure.id,
+      },
+    });
 
-
-    // todo
     const response = new AdventureCreationResponseDto();
-    // response.id = createdAdventure.id;
-    // return response;
+    response.id = adventure.id;
     return response;
   }
 }
