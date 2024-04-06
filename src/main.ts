3782f7bc5185490f21b1cfacfa675798afae95bf
Huey-J <wkdgus7113@gmail.com>
2024-04-06 19:51:21 +0900

    chore: cors 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,18 @@ async function bootstrap() {
   const config = new DocumentBuilder().setTitle('Peacetol').setDescription('API description').setVersion('1.0').build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/swagger/docs', app, document);
+
+  // CORS 설정
+  // 특정 origin만 요청 허용
+  app.enableCors({
+    // origin: appConfig.get("app.env") === "production" ? appConfig.get("app.domain") : "*",
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
+  });
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
 
   await app.listen(port);
   console.log(`listening on port ${port}`);
