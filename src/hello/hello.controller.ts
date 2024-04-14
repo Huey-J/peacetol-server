a30e9ad49f99866a9b76ea75f8b0ce606c0d71ae
Huey-J <wkdgus7113@gmail.com>
2024-04-14 13:36:12 +0900

    feat: hello controller 수정

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,22 +1,27 @@
-import { Controller, Get, Param, Req } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Param, Post, Put } from '@nestjs/common';
 import { HelloService } from './hello.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('HelloController')
 @Controller('hello')
 export class HelloController {
-
   constructor(private readonly helloService: HelloService) {}
+
+  @ApiOperation({ summary: '헬로 서비스~', description: '서비스를 호출해서 반환' })
+  @Post('with-service')
+  returnWithService(): string {
+    return this.helloService.getHello();
+  }
+
+  @ApiOperation({ summary: '헬로 서비스~', description: 'exception 반환' })
+  @Put('with-exception')
+  returnException(): string {
+    throw new HttpException('hello this is exception message', HttpStatus.FORBIDDEN);
+  }
 
   @ApiOperation({ summary: '헬로 n번 API', description: '안녕하신가요' })
   @Get(':id')
   findOne(@Param('id') id: string): string {
     return `say hello ${id} times`;
-  }
-
-  @ApiOperation({ summary: '헬로 서비스~', description: '이건 서비스를 호출해서 반환' })
-  @Get('with-service')
-  findWithService(): string {
-    return this.helloService.getHello()
   }
 }
